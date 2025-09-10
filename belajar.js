5968b5a6023b564563c532f8f501a1c66c026a92
firasrajwa <firasrajwa24@gmail.com>
2025-09-10 14:33:15 +0700

    latihanbsd

---
+++ belajar.js
@@ -0,0 +1,9 @@
+alert("Hallo Bestie!");
+confirm("Apakah kamu ingin melanjutkan?");
+prompt("Masukkan nama kamu:","Tendangan dari langit");
+function foo() { 
+alert("Ini minisoccer kak, bukan FUTSAL!!!!!");
+} foo(); a 
+Date("2024-06-12");
+parseInt("2");
+setTimeout(function(){ alert("Ini adalah alert dari setTimeout"); }, 3000);
